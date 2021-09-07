090c8d879f1142d134a61526ffdfa6efd7be7653
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2021-09-07 18:05:53 -0300

    user chart

---
+++ src/dummyData.js
@@ -0,0 +1,50 @@
+export const userData = [
+  {
+    name: 'Jan',
+    "Active User": 4000,
+  },
+  {
+    name: 'Feb',
+    "Active User": 3000,
+  },
+  {
+    name: 'Mar',
+    "Active User": 2000,
+  },
+  {
+    name: 'Apr',
+  "Active User": 2780,
+  },
+  {
+    name: 'May',
+    "Active User": 1890,
+  },
+  {
+    name: 'Jun',
+    "Active User": 2390,
+  },
+  {
+    name: 'Jul',
+    "Active User": 5090,
+  },
+  {
+    name: 'Agu',
+    "Active User": 3490,
+  },
+  {
+    name: 'Sep',
+    "Active User": 2490,
+  },
+  {
+    name: 'Oct',
+    "Active User": 1990,
+  },
+  {
+    name: 'Nov',
+    "Active User": 4490,
+  },
+  {
+    name: 'Dez',
+    "Active User": 2450,
+  },
+];
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,13 @@
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
+import Chart from "../../components/chart/Chart";
+import { userData } from "../../dummyData";
 import "./home.css";
 
 function Home() {
   return (
     <div className="home">
       <FeaturedInfo />
+      <Chart data={userData} title="User Analytics" grid dataKey="Active User" />
     </div>
   )
 }
