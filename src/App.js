249fe6c3389cae31c65b7765c39288cefe4b71e9
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2021-09-07 13:37:58 -0300

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import Topbar from  './components/topbar/Topbar';
+import Topbar from  "./components/topbar/Topbar";
+import Sidebar from  "./components/sidebar/Sidebar";
+import "./app.css";
+
 function App() {
   return (
     <div>
-      <Topbar /> 
+      <Topbar />
+      <div className="container">
+        <Sidebar /> 
+        <div className="others">Other pages</div>
+      </div>
     </div>
   );
 }
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,5 +1,5 @@
 import "./topbar.css";
-import { NotificationsNone } from '@material-ui/icons';
+import { NotificationsNone, Language, Settings } from '@material-ui/icons';
 function Topbar() {
   return (
     <div className="topbar">
@@ -8,9 +8,25 @@ function Topbar() {
           <span className="logo">Admin Section</span>
         </div>
         <div className="topRight">
-          <div className="topbarIcons">
+          <div className="topbarIconContainer">
             <NotificationsNone />
+            <span className="topIconBadge">
+              2 
+            </span>
           </div>
+          <div className="topbarIconContainer">
+            <Language />
+            <span className="topIconBadge">
+              2 
+            </span>
+          </div>
+          <div className="topbarIconContainer">
+            <Settings />
+          </div>
+          <img 
+            src="https://images.pexels.com/photos/1535244/pexels-photo-1535244.jpeg?cs=srgb&dl=pexels-hassan-ouajbir-1535244.jpg&fm=jpg" 
+            alt="" 
+            className="topAvatar" />
         </div>
       </div>
     </div>
